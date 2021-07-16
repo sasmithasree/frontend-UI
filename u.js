15b9994309fa21f1e71f3b827cb9a805e984ab6f
sasmitha sree <71595949+sasmithasree@users.noreply.github.com>
2021-07-16 16:25:43 +0530

    Add files via upload

---
+++ u.js
@@ -0,0 +1,71 @@
+
+var btn = document.getElementById("myBtn-1");
+var modal = document.getElementById("myModal-1");
+
+// Get the button that opens the modal
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-button")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+
+// Get the modal
+var modal1 = document.getElementById("edit-myModal");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("edit-myBtn");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+
+
+
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("d-close")[0];
+
+// When the user clicks the button, open the modal 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+      
+            
+        
+
+
+
+
+
+
+
